6fba32f3ed86f367d62b2d93858104ad34143250
shubhambattoo <shubham.battoo@hotmail.com>
2019-11-09 19:38:43 +0530

    review ratings handlers

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,8 @@
 // review / rating / createdAt / ref to tour / ref to user
 const mongoose = require("mongoose");
 
+const Tour = require("./tourModel");
+
 const reviewSchema = new mongoose.Schema(
   {
     review: {
@@ -39,6 +41,43 @@ reviewSchema.pre(/^find/, function(next) {
     select: "name photo"
   });
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(tourid) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourid } },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" }
+      }
+    }
+  ]);
+  // console.log(stats);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourid, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourid, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
+};
+
+reviewSchema.post("save", function() {
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  this.r = await this.findOne();
+});
+
+reviewSchema.post(/^findOneAnd/, async function(next) {
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 // eslint-disable-next-line new-cap
