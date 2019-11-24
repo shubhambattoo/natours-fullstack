4210c419cda5336769b5f7a41cbd6eff261f68e2
shubhambattoo <shubham.battoo@hotmail.com>
2019-11-24 11:57:44 +0530

    uploading user photos

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,33 @@
+const multer = require("multer");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 const factory = require("./handlerFactory");
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/img/users");
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  }
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an image! Please upload only images.", 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+const uploadUserPhoto = upload.single("photo");
 
 const filterObj = function(obj, ...allowed) {
   const newObj = {};
@@ -14,12 +40,17 @@ const filterObj = function(obj, ...allowed) {
 };
 
 const updateMe = catchAsync(async (req, res, next) => {
+  // console.log(req.file);
+  // console.log(req.body);
+
   // if the user tries to update password : ERROR
   if (req.body.password || req.body.passwordConfirm) {
     return next(new AppError("Cannot update password with this one", 400));
   }
   // update the user document
   const user = filterObj(req.body, "name", "email");
+  if (req.file) user.photo = req.file.filename;
+
   const upUser = await User.findByIdAndUpdate(req.user.id, user, {
     new: true,
     runValidators: true
@@ -67,5 +98,6 @@ module.exports = {
   deleteUser,
   updateMe,
   deleteMe,
-  getMe
+  getMe,
+  uploadUserPhoto
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,14 +9,17 @@ router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 
 router.post("/forgotPassword", authController.forgotPassword);
-
 router.patch("/resetPassword/:token", authController.resetPassword);
 
 router.use(authController.protecc);
 
 router.patch("/updateMyPassword", authController.updatePassword);
 router.get("/me", userController.getMe, userController.getUser);
-router.patch("/updateMe", userController.updateMe);
+router.patch(
+  "/updateMe",
+  userController.uploadUserPhoto,
+  userController.updateMe
+);
 router.delete("/deleteMe", userController.deleteMe);
 
 router.use(authController.restricTo("admin"));
